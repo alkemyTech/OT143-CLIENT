a7cf14775541fe79eb224c90a3ba43f85ef3fb94
Mariam Viera <mariamviera7@gmail.com>
2022-02-18 06:24:31 -0300

    feat: Creacion formulario de suscripcion a Newsletter

---
+++ src/Components/Newsletter/NewsletterForm.js
@@ -0,0 +1,74 @@
+import { Formik, Form, useField } from 'formik';
+
+const NewsletterForm = ()=>{
+  
+  const TextInput = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+      <>
+      <label>
+        {label}
+        <input {...field} {...props}/>
+      </label>
+      {meta.touched && meta.error ? (
+        <div>{meta.error}</div>
+      ) : null}
+    </>
+    );
+  };
+
+  const validate = (value)=>{
+
+    const errors = {};
+    if(value.name === ""){
+      errors.name = "Campo requerido";
+    }
+    if(value.lastname === ""){
+      errors.lastname = "Campo requerido";
+    }
+    if(value.email === ""){
+      errors.email = "Campo requerido";
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)) {
+      errors.email = 'Email invalido';
+    }
+    return errors
+  }
+  const handleSubmit = (e)=>{
+    localStorage.setItem('Newsletter', JSON.stringify(e))
+  }
+  return(
+    <>
+    {localStorage.getItem('Newsletter')? null:
+      <Formik 
+        initialValues={{
+          name: "",
+          lastname: "",
+          email: "",
+        }}
+        validate={validate}
+        onSubmit={handleSubmit}>
+        <Form >
+          <TextInput
+            name="name"
+            type="text"
+            placeholder="Nombre"
+          />
+          <TextInput
+            name="lastname"
+            type="text"
+            placeholder="Apellido"
+          />
+          <TextInput
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <button type="submit">Enviar</button>
+        </Form>
+      </Formik>
+    }  
+    </>
+  )
+}
+
+export default NewsletterForm;
