1654f6c4d2f224c4254d7e5ef06405818826ea79
Lautaro Pretti <lautaro.pretti@gmail.com>
2022-02-21 13:20:14 -0300

    Component Detail created.

---
+++ src/Components/Activities/Detail/Detail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from '../../Title';
+
+const Detail = () => {
+	const content = '';
+
+	return (
+		<>
+			<div>{Title}</div>
+			<div>{content}</div>
+		</>
+	);
+};
+
+export default Detail;
