b5bb78b4148402ae03e0f2d18b0d6562a19a08c1
Juan <juanmanuelfernandez2@outlook.com>
2022-02-22 11:25:36 -0300

    feat: Crear componente para renderizar sección Actividades

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import MembersForm from './Components/Members/MembersForm';
 import MembersEdit from './Components/Members/MembersEdit';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import NewsletterForm from './Components/Newsletter/NewsletterForm';
+import ScreenDashboard from "./Components/Dashboard/ScreenDashboard";
+import Title from './Components/Title/Title';
 
 
 function App() {
@@ -35,6 +37,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
+          <Route path="/actividades"><Title text="Actividades"/></Route>
         </Switch>
       </BrowserRouter>
     <div className="App">
