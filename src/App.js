b92d7c7b20b0ee24395cc9abe9ba991569bf70db
GuidoGagliardini <guido.hg@hotmail.com>
2022-02-22 01:37:38 -0300

    feat: Form-Ediicion/Creacion Categorias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
@@ -15,25 +14,8 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import MembersEdit from './Components/Members/MembersEdit';
 import ProjectsForm from './Components/Projects/ProjectsForm';
-=======
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
 import NewsletterForm from "./Components/Newsletter/NewsletterForm";
 import ScreenDashboard from "./Components/Dashboard/ScreenDashboard";
->>>>>>> 505e583224b21ce025df4d1f72878c323f1905ed
 
 function App() {
   return (
