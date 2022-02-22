448a6fc3c53ed602d4fc29bb71bdb5cee9301267
rodrigortigosa <rodrigortigosa@gmail.com>
2022-02-22 14:13:18 -0300

    feat: agrego titulo y obtengo categorias de la api

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../Components/FormStyles.css';
+import axios from 'axios';
 
 const NewsForm = () => {
     const [initialValues, setInitialValues] = useState({
@@ -8,6 +9,7 @@ const NewsForm = () => {
         category: '',
         image: ''
     });
+    const [categories, setCategories] = useState([]);
 
     const handleChange = (e) => {
         if(e.target.name === 'title') {
@@ -26,19 +28,28 @@ const NewsForm = () => {
         console.log(initialValues);
     }
 
+    useEffect(() => {
+        const request = axios.get("http://ongapi.alkemy.org/api/categories");
+        request.then(response => setCategories(response.data.data));
+    }, []);
+
     return (
+    <div>
+        <h2 className='title-form'>Crear/Editar novedad</h2>
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange} ></input>
             <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange} ></input>
             <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange} >
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
+                <option value="" disabled>Seleccionar categoría</option>
+                {
+                    categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)
+                }
             </select>
             <input className='input-field' type="file" name="image" accept="image/png, image/jpeg" onChange={handleChange} ></input>
-            <button className="submit-btn" type="submit">Send</button>
+            <button className="submit-btn" type="submit">Crear</button>
         </form>
+    </div>
+    
     );
 }
  
