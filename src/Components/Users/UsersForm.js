06a41b2b9637bef251023c42da3802573343120f
GermanBenavidesLeon <manchitobena@gmail.com>
2022-02-23 13:48:06 -0300

    Componente UseForm jsx y css creado para formulario creacion y edicion de usuarios

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,45 +1,142 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import '../FormStyles.css';
+import React, { Fragment, useState } from 'react';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import { BsCamera } from "react-icons/bs";
+import './UserForm.css'
 
 const UserForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        email: '',
-        roleId: ''
-    })
+    const [usuarioRegister, setUsuarioRegister] = useState(false);
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        }
+    const initialValues = {
+        profilePhoto:'',
+        name:'',
+        email:'',
+        password:'',
+        roleId:'',
     }
+    const validateFields = values => {
+        let errors = {};
+        const jpg = {};
+        const png = {};
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
+            if(!values.profilePhoto){
+                errors.profilePhoto = 'Required! '
+            }else if(!/.(jpg|png)$/i.test(values.profilePhoto)){
+                errors.profilePhoto = 'Comprueba la extensión de tus imagenes, recuerda que los formatos aceptados son .jpg y .png'
+            }
+
+            if(!values.name){
+                errors.name = 'Por favor ingrese un nombre!'
+            }else if(!/^[a-zA-ZÀ-ÿ\s]{4,40}$/.test(values.name)){
+                errors.name = 'El nombre solo puede contener letras y no menos 4 letras.'
+            }
+
+            if (!values.email) {
+                errors.email = 'Required!';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Por favor ingresra con @ !';
+            }
+
+            if (!values.password) {
+                errors.password = 'Required!';
+            }else if (values.password.length <= 8){
+                errors.password = 'El password debe tener al menos 8 digitos.';
+            }
+
+            if (!values.roleId) {
+                errors.roleId = 'Required!';
+            }
+
+            return errors;
+    }
+    
+    const handleSubmit = (valores, {resetForm}) => {
+        resetForm();
+        console.log(valores)
+        console.log('Formumaliro registrado')
+        setUsuarioRegister(true);
+        setTimeout(() => setUsuarioRegister(false), 5000)
     }
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input 
-                className="input-field" 
-                type="text" 
-                name="name" 
-                value={initialValues.name || ''} 
-                onChange={handleChange} 
-                placeholder="Name"
-            />
-            <input className="input-field" type="text" name="email" value={initialValues.description || ''} onChange={handleChange} placeholder="Email"></input>
-            <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
-                <option value="" disabled >Select the role</option>
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <Fragment>
+            <h2 className='mt-5 mx-4'>Create user</h2>
+            <Formik
+                initialValues={initialValues}
+                validate={validateFields}
+                onSubmit={handleSubmit}
+            >    
+                {({errors}) => 
+                    <Form className="row g-8 mt-4 mx-4 form">
+                        <div class="col-10 mt-2 mx-4">
+                            <div className='imageUp' type='file'>
+                                <spam className='camera'><BsCamera /></spam>
+                            </div>
+                            <div class="mb-1 mt-2">
+                                <label for="formFileSm" class="form-label">Upload your photo</label>
+                                <Field 
+                                    class="form-control form-control-sm" 
+                                    id="formFileSm"
+                                    name='profilePhoto' 
+                                    type="file"
+                                />
+                                <ErrorMessage name='profilePhoto' component={() => (
+                                    <div className='errors'>{errors.profilePhoto}</div>)}
+                                />
+                            </div>
+                                <label className='form-label'>Name</label>
+                                <Field 
+                                    className='form-control'
+                                    type="text" 
+                                    name="name"
+                                    placeholder="John Doe"
+                                />
+                                <ErrorMessage name='name' component={() => (
+                                    <div className='errors'>{errors.name}</div>)}
+                                />
+                                <label for='email'>Email</label>
+                                <Field 
+                                    className="form-control" 
+                                    type="text" 
+                                    name="email" 
+                                    placeholder="example@exam.com"
+                                />
+                                <ErrorMessage name='email' component={() => (
+                                    <div className='errors'>{errors.email}</div>)}
+                                />
+                                <label for='password'>Password</label>
+                                <Field 
+                                    className="form-control" 
+                                    type="password" 
+                                    name="password" 
+                                    placeholder="Password"
+                                />
+                                <ErrorMessage name='password' component={() => (
+                                    <div className='errors'>{errors.password}</div>)}
+                                />
+                                <label for='roleId'>Role</label>
+                                <Field 
+                                    className="form-select"
+                                    name='roleId'
+                                    as='select' 
+                                >
+                                    <option value="" disabled >Select the role</option>
+                                    <option value="adm">Admin</option>
+                                    <option value="use">User</option>
+                                </Field>
+                                <ErrorMessage name='roleId' component={() => (
+                                    <div className='errors'>{errors.roleId}</div>)}
+                                />
+                        </div>
+                                <div class="col-10 mb-3 my-3 mx-4">
+                                    <button 
+                                        type="submit"
+                                        class="btn btn-primary">User Creation</button>
+                                </div>
+                                {usuarioRegister && <p className='success'>Usuario creado exitosamente</p>}
+                    </Form>
+                }
+            </Formik>
+        </Fragment>
     );
 }
  
