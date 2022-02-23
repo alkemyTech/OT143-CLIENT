296e8ce0a3b2d1c1a399545a7b481fe648b37a31
rodrigortigosa <rodrigortigosa@gmail.com>
2022-02-23 00:29:53 -0300

    feat:  cambio los input de html por componentes de react-bootstrap

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -3,21 +3,27 @@ import "../../Components/FormStyles.css";
 import axios from "axios";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { Form, Button } from "react-bootstrap";
+
+const FORMAT_SUPPORTED = ["image/png", "image/jpg", "image/jpeg"];
+const schema = Yup.object().shape({
+  title: Yup.string()
+    .required("El título es requerido")
+    .min(4, "El título debe contener una longitud mínima de 4 caracteres"),
+  content: Yup.string().required("El contenido es requerido"),
+  category: Yup.number().required().positive("Debe seleccionar una categoría"),
+  image: Yup.mixed()
+    .required("La imagen es requerida")
+    .test(
+      "fileType",
+      "Formato de imagen inválido",
+      (value) => value && FORMAT_SUPPORTED.includes(value.type)
+    ),
+});
+
+const errorsStyles = { color: "red", fontSize: ".875em" };
 
 const NewsForm = () => {
-  const [categories, setCategories] = useState([]);
-
-  const schema = Yup.object({
-    title: Yup.string()
-      .required("El titulo es requerido")
-      .min(4, "El título debe contener una longitud mínima de 4 caracteres"),
-    content: Yup.string().required("El contenido es requerido"),
-    category: Yup.number()
-      .required()
-      .positive("Debe seleccionar una categoría"),
-    image: Yup.string().required("La imagen es requerida"),
-  });
-
   const formik = useFormik({
     initialValues: {
       title: "",
@@ -26,10 +32,12 @@ const NewsForm = () => {
       image: "",
     },
     validationSchema: schema,
-    onSubmit: (values) => {
+    onSubmit(values) {
       console.log(values);
     },
   });
+
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     const request = axios.get("http://ongapi.alkemy.org/api/categories");
@@ -39,68 +47,80 @@ const NewsForm = () => {
   return (
     <div>
       <h2 className="title-form">Crear/Editar novedad</h2>
-      <form className="form-container" onSubmit={formik.handleSubmit}>
-        <label htmlFor="title">Titulo</label>
-        <input
-          className="input-field"
-          type="text"
-          name="title"
-          value={formik.values.title || ""}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        ></input>
-        {formik.touched.title && formik.errors.title ? (
-          <div>{formik.errors.title}</div>
-        ) : null}
-        <label htmlFor="content">Contenido</label>
-        <input
-          className="input-field"
-          type="text"
-          name="content"
-          value={formik.values.content || ""}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        ></input>
-        {formik.touched.content && formik.errors.content ? (
-          <div>{formik.errors.content}</div>
-        ) : null}
-        <label htmlFor="category">Categoría</label>
-        <select
-          className="select-field"
-          name="category"
-          value={formik.values.category || 0}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        >
-          <option value={0} disabled>
-            Seleccionar categoría
-          </option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
+      <Form className="form-container" onSubmit={formik.handleSubmit}>
+        <Form.Group controlId="title" className="mb-2">
+          <Form.Label>Título</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={formik.values.title || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.title && formik.errors.title ? (
+            <div className="mt-1" style={errorsStyles}>
+              {formik.errors.title}
+            </div>
+          ) : null}
+        </Form.Group>
+        <Form.Group controlId="content" className="mb-2">
+          <Form.Label>Contenido</Form.Label>
+          <Form.Control
+            type="text"
+            name="content"
+            value={formik.values.content || ""}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.content && formik.errors.content ? (
+            <div className="mt-1" style={errorsStyles}>
+              {formik.errors.content}
+            </div>
+          ) : null}
+        </Form.Group>
+        <Form.Group controlId="category" className="mb-2">
+          <Form.Label>Categoría</Form.Label>
+          <Form.Select
+            name="category"
+            value={formik.values.category || 0}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          >
+            <option value={0} disabled>
+              Seleccionar categoría
             </option>
-          ))}
-        </select>
-        {formik.touched.category && formik.errors.category ? (
-          <div>{formik.errors.category}</div>
-        ) : null}
-        <label htmlFor="image">Imagen</label>
-        <input
-          className="input-field"
-          type="file"
-          name="image"
-          accept="image/png, image/jpeg"
-          value={formik.values.image || ""}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        ></input>
-        {formik.touched.image && formik.errors.image ? (
-          <div>{formik.errors.image}</div>
-        ) : null}
-        <button className="submit-btn" type="submit">
-          Crear
-        </button>
-      </form>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </Form.Select>
+          {formik.touched.category && formik.errors.category ? (
+            <div className="mt-1" style={errorsStyles}>
+              {formik.errors.category}
+            </div>
+          ) : null}
+        </Form.Group>
+        <Form.Group controlId="image" className="mb-2">
+          <Form.Label>Imagen</Form.Label>
+          <Form.Control
+            name="image"
+            type="file"
+            accept="image/png, image/jpg, image/jpeg"
+            onChange={(e) => {
+              formik.setFieldValue("image", e.currentTarget.files[0]);
+            }}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.image && formik.errors.image ? (
+            <div className="mt-1" style={errorsStyles}>
+              {formik.errors.image}
+            </div>
+          ) : null}
+        </Form.Group>
+
+        <Button type="submit">Crear</Button>
+      </Form>
     </div>
   );
 };
