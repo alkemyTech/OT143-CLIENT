39496de41971fc07a003e2b9dfa29cfe8a4d29b7
Juan <juanmanuelfernandez2@outlook.com>
2022-02-24 21:43:50 -0300

    feat: Creación método POST endpoints privados

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 
+const token = localStorage.getItem("token");
+
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: "143",                //Aqui va el ID del equipo!!
+        Authorization: `Bearer ${token}`
     }
 }
 
-const Get = () => {
+export const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
+    .then(res =>{
+        console.log(res.data);
+        console.log(res.status)  
+    } )
     .catch(err => console.log(err))
+}
+
+export const Post = (url, body) => {
+    axios.post(url, body, config)
+    .then(response=>console.log(response))
+    .catch(err=>console.log(err))
 }
 
 export default Get
