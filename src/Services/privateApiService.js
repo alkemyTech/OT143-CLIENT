d6ba5765960c547021eb0b28ac90db66badee8ba
Lautaro Pretti <lautaro.pretti@gmail.com>
2022-02-25 10:39:40 -0300

    patch method created

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+	headers: {
+		Group: 01, //Aqui va el ID del equipo!!
+		Auhtorization: `Bearer ${token}`,
+	},
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+export const Get = () => {
+	axios
+		.get('https://jsonplaceholder.typicode.com/users', config)
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+};
 
-export default Get
+export const Patch = (url, data, id) => {
+	axios.patch(url, data, config);
+};
