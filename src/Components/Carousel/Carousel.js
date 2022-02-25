34b41dd0f67d478dbdb0f6907c6c23d3b1c62ab3
Leonardo Coronel <leomcoronel99@gmail.com>
2022-02-25 14:30:38 -0300

    Se realiza carousel con libreria swiper.js

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,76 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import './styles.css';
-import {ReactComponent as LeftArrow} from "./img/iconmonstr-angel-left-thin.svg";
-import {ReactComponent as RightArrow} from "./img/iconmonstr-angel-right-thin.svg";
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
+import { Pagination, Navigation, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
+import "swiper/swiper.scss";
+import "swiper/modules/navigation/navigation.scss";
+import "swiper/modules/pagination/pagination.scss";
+
+import './styles.css';
+import axios from 'axios';
 const baseURL = "http://ongapi.alkemy.org/api/slides";
 
 const Carousel = () => {
-    const [images, setImages] = useState([]);    
-    const [desc, setDesc] = useState([]);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [selectedImage, setSelectedImage] = useState(images[0]);
-    const [selectedDesc, setSelectedDesc] = useState(desc[0]);
-    const [loaded, setLoaded] = useState(false);
+    const [slides, setSlides] = useState();
 
     useEffect(() => {
         axios.get(baseURL).then((response) => {
-            console.log(response.data.data.map((img) => {
-                    console.log(img)
-                    images.push(img.image);
-                    desc.push(img.description)
-                }
-            ));
+            setSlides(response.data.data);
         })
+        console.log(slides)
     }, [])
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            selectNewImage(selectedIndex, images);
-        }, 5000);
-        return () => clearInterval(interval);
-    })
-
-    const selectNewImage = (index, images, next = true) => {
-        setLoaded(false);
-        setTimeout(() => {
-            const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0;
-            const nextIndex =  next ? 
-                                    condition ? selectedIndex + 1 : 0
-                                    :
-                                    condition ? selectedIndex - 1 : images.length - 1;
-                setSelectedImage(images[nextIndex]);
-                setSelectedDesc(desc[nextIndex]);
-                setSelectedIndex(nextIndex);
-        }, 500);
-    }
-
-    const previous = () => {
-        selectNewImage(selectedIndex, images, false);
-    }
-
-    const next = () => {
-        
-        selectNewImage(selectedIndex, images);
-    }
-
     return (
-        <div className="carousel">
-            <img src={selectedImage} alt="portada" className={`carousel__portada ${loaded ? "loaded" : ""}`}  onLoad={() => setLoaded(true)} />
-            <div className="carousel__desc">
-                <p>{selectedDesc}</p>
-            </div>
-            <div className="carousel__btns">
-                <button onClick={previous}>
-                    <LeftArrow />
-                </button>
-                <button >
-                    <RightArrow onClick={next}/>
-                </button>
-            </div>
-        </div>
+        <>
+            <Swiper 
+                className='swiper'
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true
+                }}
+                navigation={true} 
+                modules={[Navigation, Pagination, Autoplay]}>
+                {!slides ? "no hay slides" : slides.map((slide, index) => (
+                    <SwiperSlide className='swiper-slide' key={`slide-${index}`}>
+                        <img src={slide.image} alt="" />
+                        <p>{slide.name}</p>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
     )
-}
+} 
 
 export default Carousel;
