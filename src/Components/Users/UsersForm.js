08976ef608305cafeb2fcbd0344bb3470f27a095
GermanBenavidesLeon <manchitobena@gmail.com>
2022-02-25 12:28:52 -0300

    en Users inclui un nuevo state

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -4,7 +4,8 @@ import { BsCamera } from "react-icons/bs";
 import './UserForm.css'
 
 const UserForm = () => {
-    const [usuarioRegister, setUsuarioRegister] = useState(false);
+    const [createUsuario, setCreateUsuario] = useState(false);
+    const [usuarioRegister] = useState(false);
 
     const initialValues = {
         profilePhoto:'',
@@ -51,89 +52,91 @@ const UserForm = () => {
         resetForm();
         console.log(valores)
         console.log('Formumaliro registrado')
-        setUsuarioRegister(true);
-        setTimeout(() => setUsuarioRegister(false), 5000)
+        setCreateUsuario(true);
+        setTimeout(() => setCreateUsuario(false), 5000)
     }
 
     return (
         <Fragment>
             <h2 className='mt-5 mx-4'>Create user</h2>
-            <Formik
-                initialValues={initialValues}
-                validate={validateFields}
-                onSubmit={handleSubmit}
-            >    
-                {({errors}) => 
-                    <Form className="row g-8 mt-4 mx-4 form">
-                        <div class="col-10 mt-2 mx-4">
-                            <div className='imageUp' type='file'>
-                                <spam className='camera'><BsCamera /></spam>
-                            </div>
-                            <div class="mb-1 mt-2">
-                                <label for="formFileSm" class="form-label">Upload your photo</label>
-                                <Field 
-                                    class="form-control form-control-sm" 
-                                    id="formFileSm"
-                                    name='profilePhoto' 
-                                    type="file"
-                                />
-                                <ErrorMessage name='profilePhoto' component={() => (
-                                    <div className='errors'>{errors.profilePhoto}</div>)}
-                                />
-                            </div>
-                                <label className='form-label'>Name</label>
-                                <Field 
-                                    className='form-control'
-                                    type="text" 
-                                    name="name"
-                                    placeholder="John Doe"
-                                />
-                                <ErrorMessage name='name' component={() => (
-                                    <div className='errors'>{errors.name}</div>)}
-                                />
-                                <label for='email'>Email</label>
-                                <Field 
-                                    className="form-control" 
-                                    type="text" 
-                                    name="email" 
-                                    placeholder="example@exam.com"
-                                />
-                                <ErrorMessage name='email' component={() => (
-                                    <div className='errors'>{errors.email}</div>)}
-                                />
-                                <label for='password'>Password</label>
-                                <Field 
-                                    className="form-control" 
-                                    type="password" 
-                                    name="password" 
-                                    placeholder="Password"
-                                />
-                                <ErrorMessage name='password' component={() => (
-                                    <div className='errors'>{errors.password}</div>)}
-                                />
-                                <label for='roleId'>Role</label>
-                                <Field 
-                                    className="form-select"
-                                    name='roleId'
-                                    as='select' 
-                                >
-                                    <option value="" disabled >Select the role</option>
-                                    <option value="adm">Admin</option>
-                                    <option value="use">User</option>
-                                </Field>
-                                <ErrorMessage name='roleId' component={() => (
-                                    <div className='errors'>{errors.roleId}</div>)}
-                                />
-                        </div>
-                                <div class="col-10 mb-3 my-3 mx-4">
-                                    <button 
-                                        type="submit"
-                                        class="btn btn-primary">User Creation</button>
+            {usuarioRegister ? <button>Ir a editar usuario</button> : 
+                <Formik
+                    initialValues={initialValues}
+                    validate={validateFields}
+                    onSubmit={handleSubmit}
+                >    
+                    {({errors}) => 
+                        <Form className="row g-8 mt-4 mx-4 form">
+                            <div class="col-10 mt-2 mx-4">
+                                <div className='imageUp' type='file'>
+                                    <spam className='camera'><BsCamera /></spam>
                                 </div>
-                                {usuarioRegister && <p className='success'>Usuario creado exitosamente</p>}
-                    </Form>
-                }
-            </Formik>
+                                <div class="mb-1 mt-2">
+                                    <label for="formFileSm" class="form-label">Upload your photo</label>
+                                    <Field 
+                                        class="form-control form-control-sm" 
+                                        id="formFileSm"
+                                        name='profilePhoto' 
+                                        type="file"
+                                    />
+                                    <ErrorMessage name='profilePhoto' component={() => (
+                                        <div className='errors'>{errors.profilePhoto}</div>)}
+                                    />
+                                </div>
+                                    <label className='form-label'>Name</label>
+                                    <Field 
+                                        className='form-control'
+                                        type="text" 
+                                        name="name"
+                                        placeholder="John Doe"
+                                    />
+                                    <ErrorMessage name='name' component={() => (
+                                        <div className='errors'>{errors.name}</div>)}
+                                    />
+                                    <label for='email'>Email</label>
+                                    <Field 
+                                        className="form-control" 
+                                        type="text" 
+                                        name="email" 
+                                        placeholder="example@exam.com"
+                                    />
+                                    <ErrorMessage name='email' component={() => (
+                                        <div className='errors'>{errors.email}</div>)}
+                                    />
+                                    <label for='password'>Password</label>
+                                    <Field 
+                                        className="form-control" 
+                                        type="password" 
+                                        name="password" 
+                                        placeholder="Password"
+                                    />
+                                    <ErrorMessage name='password' component={() => (
+                                        <div className='errors'>{errors.password}</div>)}
+                                    />
+                                    <label for='roleId'>Role</label>
+                                    <Field 
+                                        className="form-select"
+                                        name='roleId'
+                                        as='select' 
+                                    >
+                                        <option value="" disabled >Select the role</option>
+                                        <option value="adm">Admin</option>
+                                        <option value="use">User</option>
+                                    </Field>
+                                    <ErrorMessage name='roleId' component={() => (
+                                        <div className='errors'>{errors.roleId}</div>)}
+                                    />
+                            </div>
+                                    <div class="col-10 mb-3 my-3 mx-4">
+                                        <button 
+                                            type="submit"
+                                            class="btn btn-primary">User Creation</button>
+                                    </div>
+                                    {createUsuario && <p className='success'>Usuario creado exitosamente</p>}
+                        </Form>
+                    }
+                </Formik>
+            }
         </Fragment>
     );
 }
