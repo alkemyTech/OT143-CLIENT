0b659e7f7808f4c4a6bac95e2f7607bb5da030c4
GuidoGagliardini <guido.hg@hotmail.com>
2022-02-26 13:05:43 -0300

    feat:GET-PublicApi

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 const baseURL = "http://ongapi.alkemy.org/api"
 //Luego de baseURL debe ir news,categories,etc.
-export const Get = (id) => {
+export const Get = (url,id) => {
     axios.get(`${baseURL}/${id}`)
     .then(res => console.log(res))
     .catch(err => console.log(err))
