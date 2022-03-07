7f75b0a9ff3efb1268c71b2d9e48feadddc77530
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-07 14:57:09 -0300

    feat: Conecto variable de entorno con service

--- src/Services/news.js
+++ src/Services/news.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
-const BASE_URL = "http://ongapi.alkemy.org/api/news";
+const { REACT_APP_NEWS_API_ENDPOINT } = process.env;
 
 const getAll = () => {
-  const request = axios.get(BASE_URL);
+  const request = axios.get(REACT_APP_NEWS_API_ENDPOINT);
   return request.then(response => response.data.data);
 }
 
 const create = body => {
-  const request = axios.post(BASE_URL, body);
+  const request = axios.post(REACT_APP_NEWS_API_ENDPOINT, body);
   return request.then(response => response.data);
 }
 
 const update = (body, id) => {
-  const request = axios.put(`${BASE_URL}/${id}`, body);
+  const request = axios.put(`${REACT_APP_NEWS_API_ENDPOINT}/${id}`, body);
   return request.then(response => response.data);
 }
 
 const remove = id => {
-  const request = axios.delete(`${BASE_URL}/${id}`);
+  const request = axios.delete(`${REACT_APP_NEWS_API_ENDPOINT}/${id}`);
   return request.then(response => response);
 }
 
