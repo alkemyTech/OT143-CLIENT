203bf7820baf18ba42e07276f7c3e42a3ed7ef3f
GuidoGagliardini <guido.hg@hotmail.com>
2022-03-07 01:49:49 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
-<<<<<<< HEAD
-import CategoriesListBackoffice from './Components/Categories/CategoriesListBackoffice';
 import NewsForm from './Components/News/NewsForm';
-import NewsDetail from './Components/News/Detail/NewsDetail';
-=======
-import NewsForm from './Components/News/NewsForm';
->>>>>>> 28e145b32e2810142bcd2e904dcae70939936dd7
 import SlidesForm from './Components/Slides/SlidesForm';
 import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import UserForm from './Components/Users/UsersForm';
@@ -19,13 +13,8 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
-<<<<<<< HEAD
-import NewsletterForm from "./Components/Newsletter/NewsletterForm";
-import ScreenDashboard from "./Components/Dashboard/ScreenDashboard";
-=======
 import NewsletterForm from './Components/Newsletter/NewsletterForm';
 import ScreenDashboard from './Components/Dashboard/ScreenDashboard';
->>>>>>> 28e145b32e2810142bcd2e904dcae70939936dd7
 import Home from './Components/Home';
 import HomeForm from './Components/HomeForm/HomeForm';
 import Title from './Components/Title/Title';
@@ -34,47 +23,6 @@ import Detail from './Components/Activities/Detail/Detail';
 import ActivitiesList from './Components/Activities/ActivitiesList';
 import Contact from './Components/Contact/Contact';
 import OrganizationForm from './Components/Organization/OrganizationForm';
-<<<<<<< HEAD
-import MembersBackofficeList from './Components/Members/MembersBackofficeList';
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/backoffice/members/edit" component={MembersEdit} />
-          <Route
-            path="/backoffice/organization/edit"
-            component={OrganizationForm}
-          />
-          <Route path="/backoffice/members" component ={MembersBackofficeList} />
-          <Route path="/backoffice/organization" component={HomeForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/backoffice/categories" component={CategoriesListBackoffice} />
-          <Route path="/backoffice" component={ScreenDashboard} />
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/novedades/:id" exact component={NewsDetail} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/Actividades/:id" component={Detail} />
-          <Route path="/actividades">
-            <Title text="Actividades" />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-      <div className="mb-5">
-        {!localStorage.getItem("Newsletter") && <NewsletterForm />}
-      </div>
-    </>
-  );
-=======
 import NewsBackofficeList from './Components/News/NewsBackofficeList';
 import ContactForm from './Components/Contact/ContactForm';
 import News from './Components/News/News';
@@ -124,7 +72,6 @@ function App() {
 			<ContactForm />
 		</>
 	);
->>>>>>> 28e145b32e2810142bcd2e904dcae70939936dd7
 }
 
 export default App;
