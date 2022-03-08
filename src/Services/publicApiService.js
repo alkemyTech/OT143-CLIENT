d0b5f405ddff76ec8980d0f8fae428989bd980aa
GuidoGagliardini <guido.hg@hotmail.com>
2022-03-08 00:14:36 -0300

    Correcion ID GETPublicService

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 const baseURL = "http://ongapi.alkemy.org/api"
 //Luego de baseURL debe ir news,categories,etc.
 export const Get = (url,id) => {
-    axios.get(`${baseURL}/${id}`)
+    axios.get(`${baseURL}/${id ? id : ""}`)
     .then(res => console.log(res))
     .catch(err => console.log(err))
 };
