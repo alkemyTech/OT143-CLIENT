65a1a92dd2e6cae244de2c67d9d2c28e9a9ba389
Lautaro Pretti <lautaro.pretti@gmail.com>
2022-03-08 15:41:13 -0300

    task finished, awaiting review

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { FaTrashAlt } from 'react-icons/fa';
 import { FaPencilAlt } from 'react-icons/fa';
+import Loading from '../Common/Loading';
+import { successMsg, warningMsg } from '../Alerts/Alert';
 
 const ActivitiesList = () => {
 	const [actividades, setActividades] = useState('');
 
 	const getAct = async () => {
-		await axios
-			.get('http://ongapi.alkemy.org/api/activities')
-			.then(res => setActividades(res.data.data));
+		try {
+			const res = await axios.get('http://ongapi.alkemy.org/api/activities');
+			setActividades(res.data.data);
+			successMsg('Lista cargada');
+		} catch (err) {
+			warningMsg(err);
+		}
 	};
 
 	useEffect(() => {
@@ -18,73 +24,79 @@ const ActivitiesList = () => {
 		//eslint-disable-next-line
 	}, []);
 
-	return (
-		<>
-			<div className="container mt-5 mb-5">
-				<div className="row">
-					<div className="card">
-						<h1 className="d-flex justify-content-center mt-3">Actividades</h1>
-						<Link to="/create-activity">
-							<button className="btn btn-sm text-white bg-primary col-1 offset-11">
-								Create
-							</button>
-						</Link>
+	{
+		actividades === '' ? (
+			<Loading />
+		) : (
+			<>
+				<div className="container mt-5 mb-5">
+					<div className="row">
+						<div className="card">
+							<h1 className="d-flex justify-content-center mt-3">
+								Actividades
+							</h1>
+							<Link to="/create-activity">
+								<button className="btn btn-sm text-white bg-primary col-1 offset-11">
+									Create
+								</button>
+							</Link>
 
-						{actividades !== '' ? (
-							<table className="table mb-4">
-								<thead>
-									<tr>
-										<th scope="col">Name</th>
-										<th scope="col">Image</th>
-										<th scope="col">Created At</th>
-										<th scope="col">Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-									{actividades.data.map(act => {
-										return (
-											<Fragment key={act.id}>
-												<tr>
-													<th scope="row" className="col-3">
-														{act.name}
-													</th>
-													<td className="col-4">
-														<img
-															src={act.image}
-															style={{ maxWidth: '50%' }}
-															alt="Imagen de actividades"
-															className=""
-														/>
-													</td>
-													<td className="col-3">{act.created_at}</td>
-													<td className="col-2">
-														<button className="btn btn-sm bg-secondary m-3 text-white pb-2">
-															<FaPencilAlt />
-														</button>
-														<button className="btn btn-sm bg-danger m-3 text-white pb-2">
-															<FaTrashAlt />
-														</button>
-													</td>
-												</tr>
-											</Fragment>
-										);
-									})}
-								</tbody>
-							</table>
-						) : (
-							<div className="d-flex justify-content-center mt-5 mb-5">
-								<div
-									className="spinner-border d-flex justify-content-center "
-									role="status">
-									<span className="visually-hidden">Loading...</span>
+							{actividades !== '' ? (
+								<table className="table mb-4">
+									<thead>
+										<tr>
+											<th scope="col">Name</th>
+											<th scope="col">Image</th>
+											<th scope="col">Created At</th>
+											<th scope="col">Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+										{actividades.data.map(act => {
+											return (
+												<Fragment key={act.id}>
+													<tr>
+														<th scope="row" className="col-3">
+															{act.name}
+														</th>
+														<td className="col-4">
+															<img
+																src={act.image}
+																style={{ maxWidth: '50%' }}
+																alt="Imagen de actividades"
+																className=""
+															/>
+														</td>
+														<td className="col-3">{act.created_at}</td>
+														<td className="col-2">
+															<button className="btn btn-sm bg-secondary m-3 text-white pb-2">
+																<FaPencilAlt />
+															</button>
+															<button className="btn btn-sm bg-danger m-3 text-white pb-2">
+																<FaTrashAlt />
+															</button>
+														</td>
+													</tr>
+												</Fragment>
+											);
+										})}
+									</tbody>
+								</table>
+							) : (
+								<div className="d-flex justify-content-center mt-5 mb-5">
+									<div
+										className="spinner-border d-flex justify-content-center "
+										role="status">
+										<span className="visually-hidden">Loading...</span>
+									</div>
 								</div>
-							</div>
-						)}
+							)}
+						</div>
 					</div>
 				</div>
-			</div>
-		</>
-	);
+			</>
+		);
+	}
 };
 
 export default ActivitiesList;
--- src/Components/Activities/Detail/Detail.js
+++ src/Components/Activities/Detail/Detail.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Title from '../../Title/Title';
+import Loading from '../../Common/Loading';
+import { successMsg, warningMsg } from '../../Alerts/Alert';
 
 const Detail = () => {
-	const Content = '';
+	const content = '';
 
-	return (
-		<>
-			<Title text={'Detalle de actividad'} />
-			<div>{Content}</div>
-		</>
-	);
+	setTimeout(() => {
+		if (content === '') {
+			warningMsg('Error de servidor');
+		}
+	}, 10000);
+
+	if (content !== '') {
+		successMsg('Detalle cargado');
+	}
+
+	{
+		content === '' ? (
+			<Loading />
+		) : (
+			<>
+				<Title text={'Detalle de actividad'} />
+				<div>{content}</div>
+			</>
+		);
+	}
 };
 
 export default Detail;
