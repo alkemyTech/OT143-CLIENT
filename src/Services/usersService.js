773bfe7688b5a702e6cc492970feb0c2ecfc3038
Juan <juanmanuelfernandez2@outlook.com>
2022-03-07 23:07:30 -0300

    arreglados metodos de api privada

--- src/Services/usersService.js
+++ src/Services/usersService.js
@@ -1,6 +1,6 @@
 import { Get, Put, Post, Delete } from "./privateApiService";
 
-export default getUsers = () => {
+export const getUsers = () => {
   Get("http://ongapi.alkemy.org/api/users");
 };
 
@@ -8,7 +8,7 @@ export const postUser = (data) => {
   Post("http://ongapi.alkemy.org/api/users", data);
 };
 
-export const getUser = (id) => {
+export const GetUser = (id) => {
   Get(`http://ongapi.alkemy.org/api/users/${id}`);
 };
 
