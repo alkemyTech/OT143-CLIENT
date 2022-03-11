0940b136fbcd15e0dbf0b536025d93a47d71b6c0
GuidoGagliardini <guido.hg@hotmail.com>
2022-03-11 00:02:35 -0300

    feat: Skeleton/Scroll-NewsDetail & publicApiService-Fix

--- src/Components/News/Detail/NewsDetail.js
+++ src/Components/News/Detail/NewsDetail.js
@@ -1,8 +1,11 @@
-import React, { useEffect,useState } from 'react';
+import React, { useCallback, useEffect,useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Title from '../../Title/Title';
 import { useParams } from 'react-router-dom';
 import Loading from '../../Common/Loading';
+import {Events, scrollSpy} from 'react-scroll';
+import Skeleton from '../../Common/Skeleton';
+import {Get} from './../../../Services/publicApiService';
 // import axios from 'axios';
 
 //CARGA DE DATOS DE LA API.
@@ -14,18 +17,51 @@ import Loading from '../../Common/Loading';
 const NewsDetail = () => {
     const params = useParams();
     const [news,setNews] = useState();
-    const [isFetching, setIsFetching] = useState(false)
+    const [newsApi,setNewsApi] = useState();
+    const [y,setY]=useState(document.scrollingElement.scrollHeight);
+    const [scrollDirection,setScrollDirection]  = useState(false)
+    const [isFetching, setIsFetching] = useState(false);
+
+
+    const handleScroll = useCallback ((e)=>{
+        console.log(window.scrollY)
+        if (y > window.scrollY){
+            console.log("UP")
+            setScrollDirection(false)
+            window.scrollY > 150 ? setScrollDirection(true) : setScrollDirection(false)
+        }
+        else if (y < window.scrollY){
+            console.log("DOWN");
+            if (window.scrollY > 100){
+                setScrollDirection(true)     
+                try {
+                    Get(`novedades`,`${1538}`).then((res)=>{
+                        setNewsApi(res.data);
+                        setScrollDirection(false);
+                    }).catch(()=>{
+                        setScrollDirection(true);
+                        setNewsApi({error:"Intenal Server Error(500)"});
+                        setTimeout(()=>{setScrollDirection(false)},5000)
+                    });
+                } catch (error) {
+                    setScrollDirection(false)                   
+                }
+            }  
+        }
+        setY(window.scrollY);
+    },[y]);
+
+
     useEffect(()=>{
-        
-        console.log(params.id);
         setIsFetching(true);
         const cargaDetail = async () =>{
                 try {
+                    
                     const res = {
                         data :{
                             image : "IMG",
                             content : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, accusantium.",
-                            updated_at: Date()
+                            updated_at: Date(),
                         }
                     }
                     setIsFetching(false);
@@ -38,10 +74,12 @@ const NewsDetail = () => {
                 }
            
         }
-        
         cargaDetail();
-       
-    },[])
+        document.addEventListener('scroll',handleScroll);
+        return ()=>{document.removeEventListener('scroll',handleScroll)};
+    },[handleScroll]);
+
+  
     return ( <>
     
     <Title  />
@@ -60,8 +98,18 @@ const NewsDetail = () => {
                 </div>
                 <div className="text-center">
                     <h1>DetalleNovedades</h1>
-                    <p>{news.content}</p>
-                    <p>Fecha: {Date(news.updated_at)}</p>
+                    <p >{news.content}</p>
+                    <p >Fecha: {Date(news.updated_at)}</p>
+                <div className="row">
+                    <div className="col text-center">
+                        
+                {scrollDirection  ?     <Skeleton />   :  <p>{newsApi?.data}</p>}
+                        </div>
+                    
+                    </div>
+                {newsApi && scrollDirection ? newsApi.error : <h4> "Seccion Comentarios 👇"</h4>}
+                
+                
                 </div>
 
 
