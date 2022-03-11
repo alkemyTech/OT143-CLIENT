f579a8c13554579b7f153eabf44206d9636c4d48
Juan <juanmanuelfernandez2@outlook.com>
2022-03-11 18:32:42 -0300

    estilos sidebar

--- src/Components/Backoffice/Sidebar.js
+++ src/Components/Backoffice/Sidebar.js
@@ -1,8 +1,10 @@
+import { useState } from 'react'; 
 import { Container} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Sidebar.scss'
 
 const SidebarBackoffice = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const backofficeLinks = [
         {
             name: "Miembros" ,
@@ -31,8 +33,12 @@ const SidebarBackoffice = () => {
         }
     ]
 
+    const stylesContainer = { 
+        marginLeft : isOpen ? 0 : "-3000px",
+    }
+
     return ( 
-        <Container fluid="lg" className="container-links d-flex flex-column ">
+        <Container styles={stylesContainer} fluid="lg" className="container-links d-flex flex-column ">
             {
                 backofficeLinks.map(link=>{
                     return <Link className="backoffice-link p-2 ps-4" to={link.url} >{link.name}</Link>
