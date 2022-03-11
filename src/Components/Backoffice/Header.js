98249905c2d92d6b62a2a8fd1d4395389ba92d04
Juan <juanmanuelfernandez2@outlook.com>
2022-03-11 19:12:16 -0300

    toggle menu icon

--- src/Components/Backoffice/Header.js
+++ src/Components/Backoffice/Header.js
@@ -1,15 +1,32 @@
-import { Container } from 'react-bootstrap'
-import logo from './../../images/somosMasOrg.png';
-import { AiOutlineMenu } from 'react-icons/ai';
-import './Header.scss';
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import logo from "./../../images/somosMasOrg.png";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import "./Header.scss";
 
 const HeaderBackoffice = () => {
-    return (
-        <Container className='header-container' fluid>
-            <img className="header-logo ms-2 me-auto" src={logo} />
-            <AiOutlineMenu className="header-menu-icon me-4" />
-        </Container>
-    );
-}
+  const [sidebar, setSidebar] = useState(false);
+
+  const toggleSidebar = () =>{
+      setSidebar(prev => !prev)
+  }
+
+  return (
+    <Container className="header-container" fluid>
+      <img className="header-logo ms-2 me-auto" src={logo} />
+      {!sidebar ? (
+        <AiOutlineMenu
+          onClick={toggleSidebar}
+          className="header-menu-icon me-4"
+        />
+      ) : (
+        <AiOutlineClose
+          onClick={toggleSidebar}
+          className="header-menu-icon me-4"
+        />
+      )}
+    </Container>
+  );
+};
 
 export default HeaderBackoffice;
