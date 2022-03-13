4ddbffb07a9ea72f920bffb8e350f4ffb4f337b9
Leonardo Coronel <leomcoronel99@gmail.com>
2022-03-13 15:13:31 -0300

    Cards novedades en home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( <>
@@ -14,10 +16,48 @@ const Home = () => {
       
     <div className="container-fluid">
         <Carousel />
-
-        
         <div className="row text-center my-3">
             <h2 className="display-4">Últimas novedades</h2>
+        </div>
+        <div className="row justify-content-center">
+            <Card style={{ width: '18rem' }} className="m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="m-3">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+        <div className="row justify-content-center text-center my-3">
+            <div className="col-4">
+                <Link to="/novedades"><Button variant="outline-primary d-inline-block">Ver todas</Button></Link>
+            </div>
         </div>
     </div>
 
