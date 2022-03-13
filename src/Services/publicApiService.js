cc26c455694107f986703447a6ad68fc7992e52f
Christian Daniel Ortiz Orobia <cortiz@sms-sudamerica.com>
2022-03-13 17:26:14 -0300

    bugfix | Merge conflicts solved

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-
-// const config = {
-//     headers: {
-//         Group: 01 ID del equipo
-//     }
-// }
-
-=======
->>>>>>> 73bacd4b91a7a2353977158f54b324ec764ae05f
 const baseURL = "http://ongapi.alkemy.org/api"
 //Guardar en una variable de entorno BaseUrl
 export const Get = async (url,id) => {
@@ -22,8 +12,6 @@ export const Get = async (url,id) => {
     
 };
 
-<<<<<<< HEAD
-=======
 
 export const Post = (url, body) => {
     axios.post(url, body)
@@ -32,4 +20,3 @@ export const Post = (url, body) => {
 
 export default Get
 
->>>>>>> 73bacd4b91a7a2353977158f54b324ec764ae05f
