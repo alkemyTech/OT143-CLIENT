260387d770676c3958a20fb3eeaed26d9d25f198
Leonardo Coronel <leomcoronel99@gmail.com>
2022-03-13 15:03:50 -0300

    Título de novedades en Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,28 +16,9 @@ const Home = () => {
         <Carousel />
 
         
-        <div className="row ">
-            <div className="col">
-                <div class="jumbotron jumbotron-fluid bg-danger">
-                <div class="container">
-                <h1 class="display-4">Novedades ONG</h1>
-                <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, neque.</p>
-                </div>
-                </div>
-            </div>
-            <div className="col">
-            <div class="jumbotron jumbotron-fluid bg-warning">
-                <div class="container">
-                <h1 class="display-4">Titulos novedades</h1>
-                <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi rerum mollitia enim sed expedita, et tenetur? Nulla magnam earum vitae nemo dignissimos officiis ad, totam ab dolore! Suscipit porro, fugiat, sed enim quidem dolor sequi labore sit culpa rerum facere soluta sunt perspiciatis. Repellat.</p>
-                </div>
-                </div>
-
-            </div>
-
-
+        <div className="row text-center my-3">
+            <h2 className="display-4">Últimas novedades</h2>
         </div>
-        
     </div>
 
     
