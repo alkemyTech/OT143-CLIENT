c4b4d853ae73290458b84322adddfe33a5821f47
GuidoGagliardini <guido.hg@hotmail.com>
2022-03-14 21:07:22 -0300

    Fix

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,15 +2,9 @@ import React, { useEffect } from 'react';
 import Title from '../Title/Title';
 import ContactForm from './ContactForm';
 import {warningMsg} from './../Alerts/Alert';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = ({ data }) => {
-	console.log(data)
-	// const city = data.city;
-	// const address = data.address;
-	// const phone = data.phone;
-	// const email = data.email;
-
-	//COMENTO DATA PORQUE NO RECIBE DATOS.
 
 	useEffect(()=>{
 		try {
@@ -25,6 +19,18 @@ const Contact = ({ data }) => {
 		<>
 				<Title text={'Contacto'} />
 				<ContactForm />
+				{/* <MapContainer center={[51.505, -0.09]} zoom={13}>
+  				<TileLayer
+    				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  				/>
+  				<Marker position={[51.505, -0.09]}>
+    			<Popup>
+     				 A pretty CSS3 popup. <br /> Easily customizable.
+    			</Popup>
+  				</Marker>
+				</MapContainer>  */}
+				
 		</>
 	);
 };
