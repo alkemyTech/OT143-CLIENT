7f05a71b6c58950ea08cb5a3f3dbe3af9d88847a
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-15 11:18:09 -0300

    feat: Agrego rutas para donacion y gracias

--- src/config/router/PublicWebRoutes.js
+++ src/config/router/PublicWebRoutes.js
@@ -1,10 +1,12 @@
 import { Route, Switch } from "react-router-dom";
-import { ABOUT, ACTIVITIES_WEB_DETAILS, CONTACT, HOME, NEWS_WEB, NEWS_WEB_DETAILS, SCHOOL_CAMPAIGN, TOYS_CAMPAIGN } from "./routes";
+import { ABOUT, ACTIVITIES_WEB_DETAILS, CONTACT, CONTRIBUTE, HOME, NEWS_WEB, NEWS_WEB_DETAILS, SCHOOL_CAMPAIGN, THANKS, TOYS_CAMPAIGN } from "./routes";
 import SchoolCampaign from "../../Campaigns/School/SchoolCampaign";
 import ToysCampaign from "../../Campaigns/Toys/ToysCampaign";
 import ActivityDetail from "../../Components/Activities/Detail/Detail";
 import NewsDetail from "../../Components/News/Detail/NewsDetail";
 import News from "../../Components/News/News";
+import Gracias from "../../Components/Donations/Gracias";
+import Donacion from "../../Components/Donations/Donacion";
 import Contact from "../../Components/Contact/Contact";
 import AboutUs from "../../Components/About/AboutUs";
 import Home from "../../Components/Home/Home";
@@ -20,8 +22,8 @@ const PublicWebRoutes = () => {
       {/* <Route exact path={ACTIVITIES_WEB} component={Activities} /> //No existe el componente todavía */}
       <Route exact path={NEWS_WEB_DETAILS} component={NewsDetail} />
       <Route exact path={NEWS_WEB} component={News} />
-      {/* <Route exact path={THANKS} component={<Thanks />} /> //No existe el componente todavía */}
-      {/* <Route exact path={CONTRIBUTE} component={<Contribute />} /> //No existe el componente todavía */}
+      <Route exact path={THANKS} component={Gracias} />
+      <Route exact path={CONTRIBUTE} component={Donacion} />
       <Route exact path={CONTACT} component={Contact} />
       <Route exact path={ABOUT} component={AboutUs} />
       <Route exact path={HOME} component={Home} />
