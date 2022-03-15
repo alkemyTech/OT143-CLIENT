f6644f643617651763cd2318f1fb8c5a3404115c
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-15 16:50:03 -0300

    feat: Creacion de componente Footer para la web publica

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { BACKOFFICE, HOME } from './config/router/routes';
-import NewsletterForm from './Components/Newsletter/NewsletterForm';
 import BackofficeRoutes from './config/router/BackofficeRoutes';
 import PublicWebRoutes from './config/router/PublicWebRoutes';
 
@@ -15,9 +14,6 @@ function App() {
           <Route path={HOME} component={PublicWebRoutes} />
         </Switch>
       </Router>
-      <div className="mb-5">
-        {!localStorage.getItem('Newsletter') && <NewsletterForm />}
-      </div>
     </>
   );
 }
