dc21976cbe3439fd4197c511bc9936383aa802d1
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-17 20:55:04 -0300

    feat: Agrego paginado para la tabla de usuarios

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { USER_CREATE } from "../../config/router/routes";
 import { Link } from "react-router-dom";
 import { Button, Col, Container, Row } from "react-bootstrap";
@@ -6,18 +6,37 @@ import UsersTable from "./UsersTable";
 import { getUsers } from "../../features/users/usersSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../Common/Loading";
+import Pagination from "../Common/Pagination";
 
 const UsersList = () => {
 
   const { list: users, status } = useSelector(state => state.users);
   const dispatch = useDispatch();
 
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const filteredUsers = () => {
+    return users.slice(
+      currentPage,
+      currentPage + 10
+    );
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 0)
+      setCurrentPage(currentPage - 10);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 10);
+  };
+
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
   return (
-    <Container className="mt-3">
+    <Container className="my-3">
       <Row className="mb-2">
         <Col>
           <h2>Usuarios</h2>
@@ -31,7 +50,15 @@ const UsersList = () => {
       <hr />
       {
         status === "success" ?
-          <UsersTable users={users} />
+          <div>
+            <UsersTable users={filteredUsers()} />
+            <Pagination
+              onPrev={handlePrevPage}
+              onNext={handleNextPage}
+              disabledButtonPrev={currentPage === 0}
+              disabledButtonNext={filteredUsers().length < 10}
+            />
+          </div>
           :
           <div className="d-flex justify-content-center align-items-center" style={{ height: "500px" }}>
             <Loading />
