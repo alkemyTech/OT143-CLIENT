2f1c532a633382eb473aa3d3c3a278230ef7abf9
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-18 20:20:34 -0300

    fix: Cambio nombre de clase para los estilos del header de campaña escolar

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -4,25 +4,25 @@ import logo2 from './img/LOGO-SOMOS-MAS.png';
 import './Header.css';
 
 const Header = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-light bg-light">
-				<div className="container-fluid d-flex justify-content-center">
-					<div className="row d-flex  justify-content-center w-100">
-						<span className="navbar-brand col-4 col-lg-4 d-flex justify-content-center">
-							<img src={logo} alt="" className="logo1" />
-						</span>
-						<span className="navbar-brand col-3 d-none  d-xxl-flex align-items-center">
-							<h1 className="slogan">Juntos en la vuelta al cole</h1>
-						</span>
-						<span className="navbar-brand d-none d-sm-block col-4 d-sm-flex justify-content-center">
-							<img src={logo2} alt="" className="logo2" />
-						</span>
-					</div>
-				</div>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light navbar-school">
+        <div className="container-fluid d-flex justify-content-center">
+          <div className="row d-flex  justify-content-center w-100">
+            <span className="navbar-brand col-4 col-lg-4 d-flex justify-content-center">
+              <img src={logo} alt="" className="logo1" />
+            </span>
+            <span className="navbar-brand col-3 d-none  d-xxl-flex align-items-center">
+              <h1 className="slogan">Juntos en la vuelta al cole</h1>
+            </span>
+            <span className="navbar-brand d-none d-sm-block col-4 d-sm-flex justify-content-center">
+              <img src={logo2} alt="" className="logo2" />
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
