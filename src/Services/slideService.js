11049a95acfa4a2747e851135761746b89178605
Leonardo Coronel <leomcoronel99@gmail.com>
2022-03-23 19:51:34 -0300

    fix merge conflicts

--- src/Services/slideService.js
+++ src/Services/slideService.js
@@ -1,73 +1,25 @@
 import { Get, GetAll, Put, Post, Patch, Delete } from './privateApiService';
 
-<<<<<<< HEAD
-export const getData = (id)=>{
-    Get(`https://ongapi.alkemy.org/api/slides/${id}`);
-||||||| e41dc78
-export const getData = (id)=>{
-    Get(`http://ongapi.alkemy.org/api/slides/${id}`);
-=======
 export const getData = id => {
-	return Get(`http://ongapi.alkemy.org/api/slides/${id}`);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return Get(`https://ongapi.alkemy.org/api/slides/${id}`);
 };
 
-<<<<<<< HEAD
-export const getAllData = ()=>{
-    GetAll(`https://ongapi.alkemy.org/api/slides`);
-||||||| e41dc78
-export const getAllData = ()=>{
-    GetAll(`http://ongapi.alkemy.org/api/slides`);
-=======
 export const getAllData = () => {
-	return GetAll(`http://ongapi.alkemy.org/api/slides`);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return GetAll(`https://ongapi.alkemy.org/api/slides`);
 };
 
-<<<<<<< HEAD
-export const putData = (data, id)=>{
-    Put(`https://ongapi.alkemy.org/api/slides/${id}`, data);
-||||||| e41dc78
-export const putData = (data, id)=>{
-    Put(`http://ongapi.alkemy.org/api/slides/${id}`, data);
-=======
 export const putData = (data, id) => {
-	return Put(`http://ongapi.alkemy.org/api/slides/${id}`, data);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return Put(`https://ongapi.alkemy.org/api/slides/${id}`, data);
 };
 
-<<<<<<< HEAD
-export const patchData = (data, id)=>{
-    Patch(`https://ongapi.alkemy.org/api/slides/${id}`, data);
-||||||| e41dc78
-export const patchData = (data, id)=>{
-    Patch(`http://ongapi.alkemy.org/api/slides/${id}`, data);
-=======
 export const patchData = (data, id) => {
-	return Patch(`http://ongapi.alkemy.org/api/slides/${id}`, data);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return Patch(`https://ongapi.alkemy.org/api/slides/${id}`, data);
 };
 
-<<<<<<< HEAD
-export const postData = (data)=>{
-    Post(`https://ongapi.alkemy.org/api/slides`, data);
-||||||| e41dc78
-export const postData = (data)=>{
-    Post(`http://ongapi.alkemy.org/api/slides`, data);
-=======
 export const postData = data => {
-	return Post(`http://ongapi.alkemy.org/api/slides`, data);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return Post(`https://ongapi.alkemy.org/api/slides`, data);
 };
 
-<<<<<<< HEAD
-export const deleteData = (id)=>{
-    Delete(`https://ongapi.alkemy.org/api/slides/${id}`);
-||||||| e41dc78
-export const deleteData = (id)=>{
-    Delete(`http://ongapi.alkemy.org/api/slides/${id}`);
-=======
 export const deleteData = id => {
-	return Delete(`http://ongapi.alkemy.org/api/slides/${id}`);
->>>>>>> a04f55e972b4896d5190102b4d36963970ec0da3
+	return Delete(`https://ongapi.alkemy.org/api/slides/${id}`);
 };
