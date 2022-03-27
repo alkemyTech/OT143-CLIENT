d376b4789c95b45ddd5af457075d46c5cb2ae0ce
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-27 18:26:38 -0300

    Link a web publica

--- src/Components/Backoffice/Header.js
+++ src/Components/Backoffice/Header.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Link } from 'react-router-dom';
-import { BACKOFFICE } from "../../config/router/routes";
+import { HOME } from "../../config/router/routes";
 import logo from "./../../images/somosMasOrg.png";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import "./Header.scss";
@@ -28,7 +28,7 @@ const HeaderBackoffice = () => {
             className="header-menu-icon mx-4"
           />
         )}
-        <Link to={BACKOFFICE}><img className="header-logo ms-2 me-auto" src={logo} alt="Logo de la organización" /></Link>
+        <Link to={HOME}><img className="header-logo ms-2 me-auto" src={logo} alt="Logo de la organización" /></Link>
       </Container>
       <Sidebar show={showSidebar} close={toggleSidebar} />
     </>
