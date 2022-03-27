b5825229497d48a238fac1d2a0ab6f4bfb5f93ac
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-27 17:57:13 -0300

    Corrijo authSlice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,13 +4,13 @@ const token = localStorage.getItem('token');
 const headerAuthorization = token != null ? `Bearer ${token}` : null;
 
 const config = {
-    headers: {
-        Authorization: headerAuthorization,
-    },
+  headers: {
+    Authorization: headerAuthorization,
+  },
 };
 
 export const getUser = createAsyncThunk('user/getUser', async () => {
-	return fetch('https://ongapi.alkemy.org/api/auth/me', config).then((res) => res.json())
+  return fetch('https://ongapi.alkemy.org/api/auth/me', config).then((res) => res.json())
 })
 
 const tokenStorage = localStorage.getItem('token');
@@ -18,47 +18,47 @@ const tokenStorage = localStorage.getItem('token');
 // GUARDAR TOKEN Y USER EN LOCALSTORAGE
 
 const initialState = {
-	isAdmin: null,
-	status: null,
-	token: tokenStorage ? tokenStorage : null,
-	user: null,
-	auth: null,
-	role: null,
+  isAdmin: null,
+  status: null,
+  token: tokenStorage ? tokenStorage : null,
+  user: null,
+  auth: null,
+  role: null,
 };
 
 export const authSlice = createSlice({
-	name: 'auth',
-	initialState,
-	reducers: {
-		regUser: (state, action) => {
-			state.user = action.payload.user;
-			state.auth = true;
-			state.token = action.payload.token;
-		},
-		isAuth: (state, action) => {
-			state.auth = action.payload;
-		},
-		logOut: state => {
-			state.token = null;
-			state.auth = false;
-			state.user = null;
-		},
-		roleMe: (state, action) => {
-			state.role = action.payload;
-		},
-	},
-	extraReducers: {
-		[getUser.pending]: (state, action) => {
-			state.status = 'loading'
-		},
-		[getUser.fulfilled]: (state, {payload}) => {
-			state.isAdmin = payload.data.user.role_id
-			state.status = 'success'
-		},
-		[getUser.rejecter]: (state, action) => {
-			state.status = 'failed'
-		}
-	}
+  name: 'auth',
+  initialState,
+  reducers: {
+    regUser: (state, action) => {
+      state.user = action.payload.user;
+      state.auth = true;
+      state.token = action.payload.token;
+    },
+    isAuth: (state, action) => {
+      state.auth = action.payload;
+    },
+    logOut: state => {
+      state.token = null;
+      state.auth = false;
+      state.user = null;
+    },
+    roleMe: (state, action) => {
+      state.role = action.payload;
+    },
+  },
+  extraReducers: {
+    [getUser.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [getUser.fulfilled]: (state, { payload }) => {
+      state.isAdmin = token ? payload.data.user.role_id : "no hay usuario";
+      state.status = 'success'
+    },
+    [getUser.rejecter]: (state, action) => {
+      state.status = 'failed'
+    }
+  }
 });
 
 export const { regUser, isAuth, logOut, roleMe } = authSlice.actions;
