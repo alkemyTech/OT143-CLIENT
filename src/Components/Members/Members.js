fda22a17c62ab6fb20bfc1ef0ef476d33bf4bea4
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-28 03:24:23 -0300

    Muestro los miembros desde la api

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -1,10 +1,10 @@
-import ceciliaMendez from "./../../images/members/Cecilia Mendez.jpeg";
+/* import ceciliaMendez from "./../../images/members/Cecilia Mendez.jpeg";
 import marcoFernandez from "./../../images/members/Marco Fernandez.jpg";
 import mariaGarcia from "./../../images/members/María Garcia.jpg";
 import mariaIrola from "./../../images/members/María Irola.jpg";
 import maritaGomez from "./../../images/members/Marita Gomez.jpeg";
 import miriamRodriguez from "./../../images/members/Miriam Rodriguez.jpg";
-import rodrigoFuente from "./../../images/members/Rodrigo Fuente.jpg";
+import rodrigoFuente from "./../../images/members/Rodrigo Fuente.jpg"; */
 import "./Members.scss";
 import { Pagination, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
@@ -12,9 +12,14 @@ import "swiper/swiper.scss";
 import "swiper/modules/navigation/navigation.scss";
 import "swiper/modules/pagination/pagination.scss";
 import { SiFacebook, SiLinkedin } from "react-icons/si";
+import { useEffect } from "react";
+import Loading from "../Common/Loading";
+import { getMembers } from "../../features/members/membersSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Members = () => {
-  const members = [
+
+  /* const members = [
     {
       name: "Cecilia Mendez",
       image: ceciliaMendez,
@@ -71,7 +76,18 @@ const Members = () => {
       facebookUrl: "",
       linkedinUrl: "",
     },
-  ];
+  ]; */
+  const { list: members, status } = useSelector(state => state.members);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    try {
+      dispatch(getMembers());
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
 
   return (
     <Swiper
@@ -94,29 +110,34 @@ const Members = () => {
       }}
       modules={[Navigation, Pagination]}
     >
-      {members.map((member, index) => (
-        <SwiperSlide key={index} className="swiper-slide">
-          <div className="member">
-            <div>
-              <img
-                className="member-image"
-                src={member.image}
-                alt={`Miembro de la organización ${member.name}`}
-              />
-            </div>
-            <span className="member-name mt-4">{member.name}</span>
-            <p className="member-description pt-2 px-5">{member.description}</p>
-            <div>
-              <a className="social-link mx-3" href={member.facebookUrl}>
-                <SiFacebook />
-              </a>
-              <a className="social-link mx-3 " href={member.linkedinUrl}>
-                <SiLinkedin />
-              </a>
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
+      {
+        status === "success" ?
+          members.map((member, index) => (
+            <SwiperSlide key={index} className="swiper-slide">
+              <div className="member">
+                <div>
+                  <img
+                    className="member-image"
+                    src={member.image}
+                    alt={`Miembro de la organización ${member.name}`}
+                  />
+                </div>
+                <span className="member-name mt-4">{member.name}</span>
+                <p className="member-description pt-2 px-5" dangerouslySetInnerHTML={{ __html: member.description }}></p>
+                <div>
+                  <a className="social-link mx-3" href={member.facebookUrl}>
+                    <SiFacebook />
+                  </a>
+                  <a className="social-link mx-3 " href={member.linkedinUrl}>
+                    <SiLinkedin />
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))
+          :
+          <div className="d-flex justify-content-center align-items-center" style={{ height: "300px" }}><Loading /></div>
+      }
     </Swiper>
   );
 };
