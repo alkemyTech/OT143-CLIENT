244330e9a1223308566b91228304e185cf40e44d
rodrigortigosa <rodrigortigosa@gmail.com>
2022-03-28 14:25:50 -0300

    Agrego boton volver en las campañas

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo from './img/Logotipo-campaña-materiales-escolares.png';
 import logo2 from './img/LOGO-SOMOS-MAS.png';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Header = () => {
+  const history = useHistory();
   return (
     <div>
       <nav className="navbar navbar-light bg-light navbar-school">
-        <div className="container-fluid d-flex justify-content-center">
+        <div className="container-fluid">
+          <div className="row justify-content-start">
+            <div className="col">
+              <button className="btn" onClick={() => history.goBack()} >Volver</button>
+            </div>
+          </div>
           <div className="row d-flex  justify-content-center w-100">
             <span className="navbar-brand col-4 col-lg-4 d-flex justify-content-center">
               <img src={logo} alt="" className="logo1" />
@@ -17,8 +23,9 @@ const Header = () => {
               <h1 className="slogan">Juntos en la vuelta al cole</h1>
             </span>
             <span className="navbar-brand d-none d-sm-block col-4 d-sm-flex justify-content-center">
-             <Link to='/'><img src={logo2} alt="" className="logo2" /></Link> 
+              <Link to='/'><img src={logo2} alt="" className="logo2" /></Link>
             </span>
+
           </div>
         </div>
       </nav>
--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import { Navbar, Container, NavbarBrand } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {HOME} from './../../config/router/routes'
+import { Link, useHistory } from 'react-router-dom';
+import { HOME } from './../../config/router/routes'
 import './header.css';
 
 const Header = () => {
+
+  const history = useHistory();
   return (
     <header>
       <Navbar bg="light">
+        <div className="row justify-content-start">
+          <div className="col">
+            <button className="btn" onClick={() => history.goBack()} >Volver</button>
+          </div>
+        </div>
         <Container className="d-flex justify-content-xl-around">
-          <Navbar.Brand> 
-            <img src="/images/logotipo-campania-juguetes.png" 
-              width="60" 
-              height="40" 
-              className="logoCampania" 
+          <Navbar.Brand>
+            <img src="/images/logotipo-campania-juguetes.png"
+              width="60"
+              height="40"
+              className="logoCampania"
               alt="Logotipo campaña juguetes"
             />
           </Navbar.Brand>
-          <Navbar.Brand> 
-            <span className="eslogan">Juguetes por más sonrisas</span> 
+          <Navbar.Brand>
+            <span className="eslogan">Juguetes por más sonrisas</span>
           </Navbar.Brand>
           <Link to={HOME}>
-          <NavbarBrand> 
-            <img src="/images/logo-somos-mas.png"
-              width="60"
-              heigh="40"
-              className="logoSomosMas"
-              alt="Logo Somos Mas"
-            />
-          </NavbarBrand>
-          
-          </Link> 
+            <NavbarBrand>
+              <img src="/images/logo-somos-mas.png"
+                width="60"
+                heigh="40"
+                className="logoSomosMas"
+                alt="Logo Somos Mas"
+              />
+            </NavbarBrand>
+
+          </Link>
         </Container>
       </Navbar>
-    </header> 
+    </header>
   );
 }
 
