0a26f04bfbede655b20a5333f400f875a9a7c78a
Lautaro Pretti <lautaro.pretti@gmail.com>
2022-03-27 21:13:35 -0300

    changes

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import { putData, postData } from '../../Services/serviceCategories';
+import { Form, Button, Container } from 'react-bootstrap';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { Formik, Form, useField } from 'formik';
+import { useFormik } from 'formik';
 import { successMsg, warningMsg } from '../Alerts/Alert';
 import * as Yup from 'yup';
 import '../FormStyles.css';
 
-const TextInput = ({ label, foc, ...props }) => {
-	const [field, meta] = useField(props);
+const schema = Yup.object().shape({
+	title: Yup.string()
+		.required('El título es requerido')
+		.min(4, 'El título debe contener una longitud mínima de 4 caracteres'),
+	content: Yup.string().required('El contenido es requerido'),
+	image: Yup.mixed().nullable().required('La imagen es requerida'),
+});
 
-	return (
-		<>
-			<label htmlFor={props.id || props.name}>{label}</label>
-			<input className="text-input" {...field} {...props} />
-			{meta.touched && meta.error ? (
-				<div className="error alert alert-danger">{meta.error}</div>
-			) : null}
-		</>
-	);
-};
-
-const FileInput = ({ label, ...props }) => {
-	const [field, meta] = useField(props);
-
-	return (
-		<>
-			<label htmlFor={props.id || props.name}>{label}</label>
-			<input className="file-input" {...field} {...props} />
-			{meta.touched && meta.error ? (
-				<div className="error alert alert-danger">{meta.error}</div>
-			) : null}
-		</>
-	);
-};
+const errorsStyles = { color: 'red', fontSize: '.875em' };
 
 const CategoriesForm = props => {
 	const categories = props.categories
@@ -49,137 +32,129 @@ const CategoriesForm = props => {
 				image: '',
 		  };
 
-	let timeout = null;
-	let data = '';
-
-	const handleEditor = (e, editor) => {
-		const desc = editor.getData();
-		clearTimeout(timeout);
-		timeout = setTimeout(() => {
-			data = desc;
-		}, 1000);
-	};
-
-	const handleReady = editor => {
-		editor.setData(categories.content);
-	};
-
-	const createCategory = (values, id) => {
-		if (!props.categories) {
-			try {
-				postData({
-					name: values.title,
-					description: data,
-					image: values.image,
-					user_id: 0,
-					category_id: 1,
-					created_at: Date(),
-				});
-				successMsg('Creacion exitosa');
-			} catch (err) {
-				warningMsg('Creacion fallida');
-			}
-		} else {
-			try {
-				putData(
-					{
-						id: id,
+	const formik = useFormik({
+		initialValues: categories,
+		validationSchema: schema,
+		onSubmit(values, id) {
+			if (!props.news) {
+				try {
+					postData({
 						name: values.title,
-						description: data,
+						content: values.content,
 						image: values.image,
 						user_id: 0,
 						category_id: 1,
 						created_at: Date(),
-					},
-					id
-				);
-				successMsg('Edicion exitosa');
-			} catch (err) {
-				warningMsg('Edicion fallida');
+					});
+					successMsg('Creacion exitosa');
+				} catch (err) {
+					warningMsg('Creacion fallida');
+				}
+			} else {
+				try {
+					putData(
+						{
+							name: values.title,
+							content: values.content,
+							image: values.image,
+							user_id: 0,
+							category_id: 1,
+							created_at: Date(),
+						},
+						id
+					);
+					successMsg('Edicion exitosa');
+				} catch (err) {
+					warningMsg('Edicion fallida');
+				}
 			}
-		}
+		},
+	});
+
+	const convertToBase64 = file => {
+		return new Promise((resolve, reject) => {
+			const fileReader = new FileReader();
+			fileReader.readAsDataURL(file);
+			fileReader.onload = () => {
+				resolve(fileReader.result);
+			};
+			fileReader.onerror = error => {
+				reject(error);
+			};
+		}).then(base64 => {
+			return base64;
+		});
+	};
+
+	const handleImageChange = async event => {
+		const base64String = await convertToBase64(event?.target.files[0]);
+		formik.setFieldValue('image', base64String);
 	};
 
 	return (
-		<>
-			<div className="container mt-3">
-				<h2 className="title-form">{`${
-					!props.activities ? 'Crear' : 'Editar'
-				} categoria`}</h2>
-				<div className="row">
-					<div className="mt-5 pt-3">
-						<Formik
-							initialValues={categories}
-							validationSchema={Yup.object({
-								title: Yup.string()
-									.required('Ingresar titulo')
-									.max(4, 'Máximo 4 caracteres'),
-								image: Yup.mixed()
-									.required('Ingresar imagen')
-									.test('fileType', 'Unsupported File Format', value => {
-										if (value) {
-											if (value.includes('png')) {
-												return true;
-											} else if (value.includes('jpg')) {
-												return true;
-											} else if (value.includes('jpeg')) {
-												return true;
-											} else {
-												return false;
-											}
-										}
-									}),
-							})}
-							onSubmit={(values, { setFieldValue }) => {
-								createCategory(values);
-								// setFieldValue('title', '');
-								// setFieldValue('image', '');
-							}}>
-							<Form>
-								<TextInput
-									label="Título"
-									name="title"
-									type="text"
-									className="form-control mt-3 mb-3"
-								/>
-								<div className="mb-3">
-									<div className="mb-3">
-										<span>Descripción</span>
-									</div>
+		<Container className="mt-3">
+			<h2 className="title-form">{`${
+				!props.news ? 'Crear' : 'Editar'
+			} categoria`}</h2>
+			<div className="mt-5">
+				<Form className="form" onSubmit={formik.handleSubmit}>
+					<Form.Group controlId="title" className="mt-2 mb-3">
+						<Form.Label>Título</Form.Label>
+						<Form.Control
+							type="text"
+							name="title"
+							value={formik.values.title || ''}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+						/>
+						{formik.touched.title && formik.errors.title ? (
+							<div className="mt-1" style={errorsStyles}>
+								{formik.errors.title}
+							</div>
+						) : null}
+					</Form.Group>
 
-									<CKEditor
-										label="Descripción"
-										editor={ClassicEditor}
-										data=""
-										name="description"
-										type="text"
-										placeholder="Descripción"
-										onChange={handleEditor}
-										onReady={handleReady}
-										className="form-control"
-									/>
-								</div>
+					<Form.Group controlId="content" className="mt-3 mb-3">
+						<Form.Label>Descripcion</Form.Label>
+						<CKEditor
+							placeholder="Contenido"
+							editor={ClassicEditor}
+							data={formik.values.content || ''}
+							name="content"
+							type="text"
+							onChange={(e, editor) =>
+								formik.setFieldValue('content', editor.getData())
+							}
+						/>
+						{formik.touched.content && formik.errors.content ? (
+							<div className="mt-1" style={errorsStyles}>
+								{formik.errors.content}
+							</div>
+						) : null}
+					</Form.Group>
 
-								<FileInput
-									label="Imagen"
-									name="image"
-									type="file"
-									placeholder="Imagen"
-									accept=".jpg, .jpeg, .png"
-									className="form-control mt-3 mb-3"
-								/>
+					<Form.Group controlId="image" className="mt-3 mb-3">
+						<Form.Label>Imagen</Form.Label>
+						<Form.Control
+							name="image"
+							type="file"
+							accept="image/png, image/jpeg, image/jpg"
+							onChange={event => handleImageChange(event)}
+							onBlur={formik.handleBlur}
+						/>
+						{formik.touched.image && formik.errors.image ? (
+							<div className="mt-1" style={errorsStyles}>
+								{formik.errors.image}
+							</div>
+						) : null}
+					</Form.Group>
 
-								<button
-									type="submit"
-									className="form-control btn btn-primary mt-3 mb-3">
-									{!props.categories ? 'Crear' : 'Editar'}
-								</button>
-							</Form>
-						</Formik>
-					</div>
-				</div>
+					<Button type="submit" className="w-100 mb-2">
+						{!props.news ? 'Crear' : 'Editar'}
+					</Button>
+				</Form>
 			</div>
-		</>
+		</Container>
 	);
 };
 
